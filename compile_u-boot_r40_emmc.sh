c6ad27b9e6b2146a38f18e9a5f8d1964fb9ce467
juanesf <juanesf91@gmail.com>
2020-03-10 11:43:36 -0300

    add compile scripts for R40

---
+++ compile_u-boot_r40_emmc.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+###compile uboot m2u
+echo "\033[36m Compile U-boot emmc for M2U(R40).\033[0m"
+echo "\033[36m Installing dependencies.\033[0m"
+sudo apt update && sudo apt install -y swig python-dev python3-dev git
+echo "\033[36m changing folder.\033[0m"
+cd /tmp
+echo "\033[36m cloning u-boot.\033[0m"
+git clone https://github.com/juanesf/u-boot.git && cd u-boot && make Bananapi_M2_Ultra_defconfig && make -j $(nproc)
+echo "\033[36m make boot.scr.\033[0m"
+mkimage -C none -A arm -T script -d /tmp/u-boot/r40_boot.cmd boot.scr
+echo "\033[36m copying binary u-boot emmc.\033[0m"
+sudo dd if=/tmp/u-boot/u-boot-sunxi-with-spl.bin of=/dev/mmcblk1p0 bs=1024 seek=8
+echo "\033[36m removing dtb.\033[0m"
+sudo rm /boot/dtb/sun8i-r40-bananapi-m2-ultra.dtb
+echo "\033[36m copying new dtb emmc.\033[0m"
+sudo cp /tmp/u-boot/arch/arm/dts/sun8i-r40-bananapi-m2-ultra.dtb /boot/dtb
+echo "\033[36m Please reboot Now.\033[0m"
+
+
+
+###dtc -I dtb -O dts /tmp/u-boot/arch/arm/dts/sun8i-r40-bananapi-m2-ultra.dtb -o /tmp/tmp.dts && less /tmp/tmp.dts
+
+
+
+###rm -Rf u-boot
+###git clone https://github.com/juanesf/u-boot.git && cd u-boot && make CROSS_COMPILE=arm-linux-gnueabihf- Bananapi_M2_Ultra_defconfig && make -j $(nproc) CROSS_COMPILE=arm-linux-gnueabihf-
+###dtc -I dtb -O dts /tmp/u-boot/arch/arm/dts/sun8i-r40-bananapi-m2-ultra.dtb -o /tmp/tmp.dts
+###less /tmp/tmp.dts
+
+###sudo cp /lib/firmware/ap6212/nvram.txt /lib/firmware/brcm/brcmfmac43430-sdio.sinovoip,bpi-m2-ultra.txt
+###sudo cp /lib/firmware/ap6212/fw_bcm43438a1.bin /lib/firmware/brcm/brcmfmac43430-sdio.bin
+###sudo cp /lib/firmware/ap6212/bcm43438a0.hcd /lib/firmware/brcm/bcm43430a1.hcd
