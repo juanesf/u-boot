dd451b89471c108350be405c32268d02d603fef3
juanesf <juanesf91@gmail.com>
2020-06-18 19:22:51 -0400

    Sync with kernel 5.4.47

--- include/dt-bindings/clock/sun8i-r40-ccu.h
+++ include/dt-bindings/clock/sun8i-r40-ccu.h
@@ -43,7 +43,28 @@
 #ifndef _DT_BINDINGS_CLK_SUN8I_R40_H_
 #define _DT_BINDINGS_CLK_SUN8I_R40_H_
 
-#define CLK_CPU			24
+#define CLK_PLL_AUDIO_BASE	2
+#define CLK_PLL_AUDIO		3
+#define CLK_PLL_AUDIO_2X	4
+#define CLK_PLL_AUDIO_4X	5
+#define CLK_PLL_AUDIO_8X	6
+#define CLK_PLL_VIDEO0		7
+#define CLK_PLL_VIDEO0_2X	8
+#define CLK_PLL_VE			9
+
+#define CLK_PLL_PERIPH0		11
+#define CLK_PLL_PERIPH0_SATA	12
+#define CLK_PLL_PERIPH0_2X	13
+#define CLK_PLL_PERIPH1		14
+#define CLK_PLL_PERIPH1_2X	15
+#define CLK_PLL_VIDEO1		16
+#define CLK_PLL_VIDEO1_2X	17
+
+#define CLK_CPU				24
+#define CLK_AXI				25
+#define CLK_AHB1			26
+#define CLK_APB1			27
+#define CLK_APB2			28
 
 #define CLK_BUS_MIPI_DSI	29
 #define CLK_BUS_CE		30
@@ -172,7 +193,7 @@
 #define CLK_AVS			152
 #define CLK_HDMI		153
 #define CLK_HDMI_SLOW		154
-
+#define CLK_MBUS		155
 #define CLK_DSI_DPHY		156
 #define CLK_TVE0		157
 #define CLK_TVE1		158
